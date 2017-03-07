68011b6bae7256715b353ff235a50def44a65361
Tahlil <calicratis19@gmail.com>
2017-03-07 18:22:26 +0600

    Very bad performance so far. Not sure dice off coefficient error function is the right choice.

--- CarND-Vehicle Detection and Tracking/Vehicle_Detection.py
+++ CarND-Vehicle Detection and Tracking/Vehicle_Detection.py
@@ -9,6 +9,7 @@ import pickle
 from skimage.feature import hog
 from sklearn.utils import shuffle
 
+from keras import backend as K
 from keras.models import Sequential
 from keras.models import Model
 from keras.layers import ELU
@@ -31,60 +32,73 @@ csvFile = pd.read_csv(rootDir+'/labels.csv', header=0)
 dataFile = csvFile[(csvFile['Label']!='Pedestrian')].reset_index()
 dataFile = dataFile.drop('index', 1)
 dataFile = dataFile.drop('Preview URL', 1)
-#dataFile['File_Path'] =  rootDir + '/' +dataFile['Frame']
 dataFile.head()
 
-train_samples_per_epoch = 2560
+train_samples_per_epoch = 10000
 valid_samples_per_epoch = 16384
-trainBatchSize = 8
+trainBatchSize = 16
 validationBatchSize = 8
-imgRow = 424
-imgCol = 640
+imgRow = 512
+imgCol = 768
+smooth = 1.
 
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
 
 def TrainDataGenerator(dataInfo, batchSize, rootDir):
     batch_x, batch_y = [], []
     while True:
-        row = 0
-        while row < len(dataInfo):
+        row = np.random.randint(len(dataInfo))
+        #while row < len(dataInfo):
 
-            fileName = './' + rootDir + '/' + dataInfo['Frame'][row]
-            # print(fileName, row)
-            img = cv2.imread(fileName)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            origShape = img.shape
-            img = cv2.resize(img, (imgCol, imgRow))
+        fileName = './' + rootDir + '/' + dataInfo['Frame'][row]
+        # print(fileName, row)
+        img = cv2.imread(fileName)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        origShape = img.shape
+        img = cv2.resize(img, (imgCol, imgRow))
 
-            data = dataInfo[dataInfo['Frame'][row] == dataInfo['Frame']].reset_index()
-            data['xmin'] = np.round(data['xmin'] / origShape[1] * imgCol)
-            data['xmax'] = np.round(data['xmax'] / origShape[1] * imgCol)
-            data['ymin'] = np.round(data['ymin'] / origShape[0] * imgRow)
-            data['ymax'] = np.round(data['ymax'] / origShape[0] * imgRow)
+        data = dataInfo[dataInfo['Frame'][row] == dataInfo['Frame']].reset_index()
+        data['xmin'] = np.round(data['xmin'] / origShape[1] * imgCol)
+        data['xmax'] = np.round(data['xmax'] / origShape[1] * imgCol)
+        data['ymin'] = np.round(data['ymin'] / origShape[0] * imgRow)
+        data['ymax'] = np.round(data['ymax'] / origShape[0] * imgRow)
 
-            targetImg = np.reshape(np.zeros_like(img[:, :, 2]), (imgRow, imgCol, 1))
-            for i in range(len(data)):
-                targetImg[data.iloc[i]['xmax']:data.iloc[i]['ymax'], data.iloc[i]['xmin']:data.iloc[i]['ymin']] = 1
-                cv2.rectangle(img, (int(data.iloc[i]['xmin']), int(data.iloc[i]['xmax'])),
-                              (int(data.iloc[i]['ymin']), int(data.iloc[i]['ymax'])), (0, 0, 255), 6)
-                # print(data.iloc[i]['xmin'],data.iloc[i]['xmax'], data.iloc[i]['ymin'],data.iloc[i]['ymax'])
+        targetImg = np.reshape(np.zeros_like(img[:, :, 2]), (imgRow, imgCol, 1))
+        for i in range(len(data)):
+            targetImg[data.iloc[i]['xmax']:data.iloc[i]['ymax'], data.iloc[i]['xmin']:data.iloc[i]['ymin']] = 1
+            cv2.rectangle(img, (int(data.iloc[i]['xmin']), int(data.iloc[i]['xmax'])),
+                          (int(data.iloc[i]['ymin']), int(data.iloc[i]['ymax'])), (0, 0, 255), 6)
+            # print(data.iloc[i]['xmin'],data.iloc[i]['xmax'], data.iloc[i]['ymin'],data.iloc[i]['ymax'])
 
-            row += len(data) - 1
+        row += len(data) - 1
 
-            batch_x.append(img)
-            batch_y.append(targetImg)
+        batch_x.append(img/127.5 - 1.)
+        batch_y.append(targetImg)
 
-            if (len(batch_x) == batchSize):
-                # yield (np.vstack(batch_x),np.vstack(batch_y))
-                x_array = np.asarray(batch_x)
-                y_array = np.asarray(batch_y)
-                yield (x_array, y_array)
-                batch_x, batch_y = [], []
-            row += 1
+        if (len(batch_x) == batchSize):
+            # yield (np.vstack(batch_x),np.vstack(batch_y))
+            x_array = np.asarray(batch_x)
+            y_array = np.asarray(batch_y)
+            yield (x_array, y_array)
+            batch_x, batch_y = [], []
+        row += 1
 
 
 def CreateModel():
     input_layer = Input((imgRow, imgCol, 3))
-    conv1 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(input_layer)
+    conv0 = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(input_layer)
+    conv0 = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(conv0)
+    pool0 = MaxPooling2D(pool_size=(2, 2))(conv0)
+
+    conv1 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(pool0)
     conv1 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
@@ -115,11 +129,15 @@ def CreateModel():
     conv7 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(up7)
     conv7 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(conv7)
 
-    conv8 = Convolution2D(1, 1, 1, activation='sigmoid')(conv7)
+    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv0], mode='concat', concat_axis=3)
+    conv8 = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(up8)
+    conv8 = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(conv8)
 
-    model = Model(input=input_layer, output=conv8)
+    conv9 = Convolution2D(1, 1, 1, activation='sigmoid')(conv8)
 
-    model.compile(optimizer=Adam(lr=1e-4), loss="mse", metrics=['accuracy'])
+    model = Model(input=input_layer, output=conv9)
+
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
 
     return model
 
@@ -127,14 +145,15 @@ def CreateModel():
 model = CreateModel()
 
 trainGenerator = TrainDataGenerator(dataFile, trainBatchSize, rootDir)
-# trainGenerator = TrainDataGenerator(trainBatchSize)
 
 weight_save_callback = ModelCheckpoint('./weights/weights.{epoch:02d}-{loss:.4f}.h5', monitor='loss', verbose=2,
                                        save_best_only=False, mode='auto')
 model.summary()
 
+#model.load_weights('model.h5')
+
 print("Created generator and call backs. Starting training")
-'''
+
 model.fit_generator(
     trainGenerator,
     samples_per_epoch=train_samples_per_epoch, nb_epoch=40,
@@ -142,7 +161,7 @@ model.fit_generator(
     # nb_val_samples=valid_samples_per_epoch,
     callbacks=[weight_save_callback],
     verbose=1
-)'''
+)
 
 model.save_weights('model.h5', True)
 with open('model.json', 'w') as file:
